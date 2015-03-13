4735166bfc83651d46ee2ee4d319405b8df4f7ed
lanrion <huaitao-deng@foxmail.com>
2015-03-13 14:30:45 +0800

    调整需求card管理页面

--- app/admin/card.rb
+++ app/admin/card.rb
@@ -3,6 +3,9 @@ ActiveAdmin.register Card do
   permit_params :name, :description, :finished_hour, checklists_attributes: [:_destroy,
     :id, :name, :is_finished, :finished_at, :finished_hour]
 
+    filter :name
+    filter :created_at
+
   index do
     selectable_column
     id_column
@@ -13,6 +16,25 @@ ActiveAdmin.register Card do
     actions
   end
 
+  show do |card|
+    attributes_table do
+      Card.attribute_names.each do |attr_name|
+        row attr_name
+      end
+    end
+
+    panel "任务列表" do
+      paginated_collection(card.checklists.page(params[:page]).per(15)) do
+        table_for(collection) do
+          CheckList.attribute_names.each do |attr_name|
+            next if attr_name == "card_id"
+            column CheckList.human_attribute_name(attr_name), attr_name
+          end
+        end
+      end
+    end
+  end
+
   form do |f|
     f.inputs do
       f.input :name
@@ -20,12 +42,12 @@ ActiveAdmin.register Card do
       f.input :finished_hour
     end
 
-    f.inputs "Check List" do
+    f.inputs "任务列表" do
       f.has_many :checklists, heading: false, allow_destroy: true do |checklist|
         checklist.semantic_errors *checklist.object.errors.keys
         checklist.input :name
         checklist.input :finished_hour
-        checklist.input :finished_at, as: :datetime_picker
+        checklist.input :finished_at
         checklist.input :is_finished
       end
     end
