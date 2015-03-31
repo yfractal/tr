06fbe4db9ecbbffdc3070528e89b974f2fcaed25
lanrion <huaitao-deng@foxmail.com>
2015-03-31 23:35:04 +0800

    优化项目与需求跳转交互

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,20 +1,35 @@
 ActiveAdmin.register Project do
-
+  # actions :all, except: :show
 
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   permit_params "name", "desc", "company_id", "play_site", "production_site", "hour_fee"
 
+  filter :name
+  filter :company
+  filter :hour_fee
+  filter :total_hours
+  filter :created_at
+  filter :updated_at
+
   index do
-    column :name
+    column :name do |pro|
+      link_to pro.name, admin_card_path(pro)
+    end
     column :company
     column :hour_fee
     column :total_hours
     column :total_amount
     column :created_at
     column :updated_at
-    actions
+    actions do |pro|
+      link_to "需求", admin_cards_path("q[project_id_eq]" => pro.id)
+    end
+  end
+
+  action_item :show, only: :show do
+    link_to "需求列表", admin_cards_path("q[project_id_eq]" => resource.id)
   end
 
   show do |pro|
